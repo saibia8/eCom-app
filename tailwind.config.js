782affc59dbe3c4a8219ae1369f318f97ed47510
saibia8 <saibia.design@gmail.com>
2024-01-01 18:42:54 +0100

    Install daisyui

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,13 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    'node_modules/daisyui/dist/**/*.js',
+    'node_modules/react-daisyui/dist/**/*.js',
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  // eslint-disable-next-line no-undef
+  plugins: [require("daisyui")],
 }
 
